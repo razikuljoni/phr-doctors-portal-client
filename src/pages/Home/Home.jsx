0ad967e2a1f87294f42e01cacef2b32e28032c35
Md Razikul Islam Joni <mdrazikulislamjoni@gmail.com>
2022-07-27 19:27:06 +0600

    make footer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../../assets/components/Banner/Banner';
 import ContactUs from '../../assets/components/ContactUs/ContactUs';
+import Footer from '../../assets/components/Footer/Footer';
 import Info from '../../assets/components/InfoCard/Info/Info';
 import MakeAppointment from '../../assets/components/MakeAppointment/MakeAppointment';
 import Services from '../../assets/components/Services/Services/Services';
@@ -15,6 +16,7 @@ const Home = () => {
             <MakeAppointment/>
             <Testimonials/>
             <ContactUs/>
+            <Footer/>
         </div>
     );
 };
